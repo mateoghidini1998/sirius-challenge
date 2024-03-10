d755f9b7112e63bfa59a50d1ac8f15d9f991bae6
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-10 13:30:45 -0300

    Create a util set of common words

---
+++ utils/commonWords.js
@@ -0,0 +1,3 @@
+const commonWords = new Set(['a', 'the', 'is', 'in', 'an', 'and', 'of', 'for', 'on', 'at', 'with', 'to', 'your', 'it', 'you', 'or', 'this', 'above', 'about', 'below', 'between', 'beneath', 'down', 'from', 'off', 'like', 'near', 'behind', 'over', 'through', 'up', 'under', 'upon', 'without', 'till', 'throughout', 'into', 'but', 'by', 'among', 'are', 'thoughtfully', 'our', 'us', 'as', 'let' ]);
+
+module.exports = commonWords;
