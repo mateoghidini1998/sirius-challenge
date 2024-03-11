092b02b6d89e746fcaabf26554baf9548513a500
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-11 10:46:11 -0300

    Create a util function for words to be excluded

--- utils/commonWords.js
+++ utils/commonWords.js
@@ -1,3 +1,8 @@
 const commonWords = new Set(['a', 'the', 'is', 'in', 'an', 'and', 'of', 'for', 'on', 'at', 'with', 'to', 'your', 'it', 'you', 'or', 'this', 'above', 'about', 'below', 'between', 'beneath', 'down', 'from', 'off', 'like', 'near', 'behind', 'over', 'through', 'up', 'under', 'upon', 'without', 'till', 'throughout', 'into', 'but', 'by', 'among', 'are', 'thoughtfully', 'our', 'us', 'as', 'let' ]);
 
-module.exports = commonWords;
+const shouldExcludeWord = (word) => {
+    return commonWords.has(word) || !isNaN(word);
+};
+
+
+module.exports = shouldExcludeWord;
