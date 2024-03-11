5e93b639998bc86112af5935115c293ea3363796
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-11 10:46:39 -0300

    Import and implement utils functions

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,13 @@
-const Redis = require('ioredis');
-const redis = new Redis();
 const ErrorResponse = require('../utils/errorResponse')
-const commonWords = require('../utils/commonWords')
+const shouldExcludeWord  = require('../utils/commonWords')
 const asyncHandler = require('../middleware/async')
 const scrapeUrl = require('../utils/scrapeUrl')
-
+const redisCache = require('../utils/redisCache')
 
 //@route   POST /product-word-cloud
 //@desc    Create product word cloud
 //@access  Public
 exports.getData = asyncHandler(async (req, res, next) => {
-    const shouldExcludeWord = (word) => {
-        return commonWords.has(word) || !isNaN(word);
-    }
-
     const url = req.body.url;
 
     //Check if there is an url
@@ -22,25 +16,19 @@ exports.getData = asyncHandler(async (req, res, next) => {
     }
 
     //Verify if the urls are already stored in cache.
-    let cachedData = await redis.get(url);
+    let cachedData = await redisCache.getFromCache(url);
     if (cachedData) {
         console.log("Data retrieved from cache");
-        return res.send(JSON.parse(cachedData))
+        return res.send(cachedData)
     } else {
-        
         //Target the selector to scrap from the product.
         const description = await scrapeUrl(url);
-
         //Sort and filter the words from the description.
         const words = description.split(/\W+/).map(word => word.toLowerCase()).filter(word => word && word.length > 1 && !shouldExcludeWord(word)).slice(0, 20);
-        console.log(words);
         const wordCount = words.reduce((counts, word) => ({...counts, [word]: (counts[word] || 0) + 1}), {});
-
         const wordCloud = Object.entries(wordCount).sort((a, b) => b[1] - a[1]).slice(0, 10);
-
         //Set a key and value to store in cache
-        await redis.set(url, JSON.stringify(wordCloud));
-
+        await redisCache.setToCache(url, 3600, wordCloud);
         return res.send(wordCloud);
     }
 });
