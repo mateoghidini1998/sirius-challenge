652e34d8fbe863cfc7122efb5149d4b728d55126
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-11 20:39:00 -0300

    Add an updated set of words

--- utils/commonWords.js
+++ utils/commonWords.js
@@ -1,4 +1,4 @@
-const commonWords = new Set(['a', 'the', 'is', 'in', 'an', 'and', 'of', 'for', 'on', 'at', 'with', 'to', 'your', 'it', 'you', 'or', 'this', 'above', 'about', 'below', 'between', 'beneath', 'down', 'from', 'off', 'like', 'near', 'behind', 'over', 'through', 'up', 'under', 'upon', 'without', 'till', 'throughout', 'into', 'but', 'by', 'among', 'are', 'thoughtfully', 'our', 'us', 'as', 'let', 'its', 'any' ]);
+const commonWords = new Set(['a', 'the', 'is', 'in', 'an', 'and', 'of', 'for', 'on', 'at', 'with', 'to', 'your', 'it', 'you', 'or', 'this', 'above', 'about', 'below', 'between', 'beneath', 'down', 'from', 'off', 'like', 'near', 'behind', 'over', 'through', 'up', 'under', 'upon', 'without', 'till', 'throughout', 'into', 'but', 'by', 'among', 'are', 'thoughtfully', 'our', 'us', 'as', 'let', 'its' ]);
 
 const shouldExcludeWord = (word) => {
     return commonWords.has(word) || !isNaN(word);
