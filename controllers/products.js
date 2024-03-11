da1e4d373cd8136b1eb0782bd940c4be30fbc7f8
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-11 13:00:33 -0300

    Send url through param, and get the words from redis set

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const redisCache = require('../utils/redisCache')
 //@desc    Create product word cloud
 //@access  Public
 exports.getData = asyncHandler(async (req, res, next) => {
-    const url = req.body.url;
+    const url = req.query.url;
 
     //Check if there is an url
     if (!url) {
@@ -24,11 +24,18 @@ exports.getData = asyncHandler(async (req, res, next) => {
         //Target the selector to scrap from the product.
         const description = await scrapeUrl(url);
         //Sort and filter the words from the description.
-        const words = description.split(/\W+/).map(word => word.toLowerCase()).filter(word => word && word.length > 1 && !shouldExcludeWord(word)).slice(0, 20);
+        const words = description.split(/\W+/)
+            .map(word => word.toLowerCase())
+            .filter(word => word && word.length > 1 && !shouldExcludeWord(word))
+            .slice(0, 100);
+
+        await redisCache.addWordsToSet(words);
+        const allWordsInSet = await redisCache.getWordsFromSet();
+        console.log(allWordsInSet);
         const wordCount = words.reduce((counts, word) => ({...counts, [word]: (counts[word] || 0) + 1}), {});
         const wordCloud = Object.entries(wordCount).sort((a, b) => b[1] - a[1]).slice(0, 10);
         //Set a key and value to store in cache
-        await redisCache.setToCache(url, 3600, wordCloud);
+        await redisCache.setToCache(url, 60, wordCloud);
         return res.send(wordCloud);
     }
 });
