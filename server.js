0ae19c28b51e956f797a09a4e4089e98a4639e0b
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-11 23:50:37 -0300

    Verify if the env is in test mode

--- server.js
+++ server.js
@@ -48,9 +48,11 @@ dotenv.config({ path: './config/config.env' });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    });
+};
 
 
 module.exports = app;
