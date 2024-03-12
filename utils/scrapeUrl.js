18f4a8330669d8a23a18f77eab170d0a353178f6
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-11 23:50:51 -0300

    Send error if no url received

--- utils/scrapeUrl.js
+++ utils/scrapeUrl.js
@@ -3,6 +3,11 @@ const cheerio = require("cheerio");
 
 const scrapeUrl = async (url) => {
     try {
+
+        if (!url) {
+            throw new Error("URL is required");
+        }
+
         const response = await axios.get(url, {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
